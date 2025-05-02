79dec478ec004fe54a4b225cc65450fa05390ffd
Aleksandr <kraichik03@mail.ru>
2025-05-02 12:36:11 +0300

    feat: временное решение для проверки контрллеров, в будущем все вынести и отрефакторить

--- Otus.TMS/Otus.TMS/Program.cs
+++ Otus.TMS/Otus.TMS/Program.cs
@@ -1,7 +1,66 @@
 using Microsoft.EntityFrameworkCore;
+using Otus.TMS.Controllers;
 using Otus.TMS.Infrastructure.DataAccess.Contexts;
+using Otus.TMS.Infrastructure.DataAccess.DataModels;
+using Otus.TMS.Infrastructure.DataAccess.Interfaces;
+using Otus.TMS.Infrastructure.DataAccess.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Регистрация репозиториев
+builder.Services.AddScoped<IAuditableCommandRepository<Board>, BoardRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Board>, BoardRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<BoardUser>, BoardUserRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<BoardUser>, BoardUserRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<BoardUserRole>, BoardUserRoleRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<BoardUserRole>, BoardUserRoleRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Column>, ColumnRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Column>, ColumnRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Comment>, CommentRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Comment>, CommentRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Company>, CompanyRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Company>, CompanyRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<CredentialHistory>, CredentialHistoryRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<CredentialHistory>, CredentialHistoryRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Credential>, CredentialRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Credential>, CredentialRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Department>, DepartmentRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Department>, DepartmentRepository>();
+
+builder.Services.AddScoped<ICommandRepository<NotificationSetting>, NotificationSettingRepository>();
+builder.Services.AddScoped<IQueryRepository<NotificationSetting>, NotificationSettingRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Permission>, PermissionRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Permission>, PermissionRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<RolePermission>, RolePermissionRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<RolePermission>, RolePermissionRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Role>, RoleRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Role>, RoleRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<Otus.TMS.Infrastructure.DataAccess.DataModels.Task>, TaskRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<Otus.TMS.Infrastructure.DataAccess.DataModels.Task>, TaskRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<TaskType>, TaskTypeRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<TaskType>, TaskTypeRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<TelegramAccount>, TelegramAccountRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<TelegramAccount>, TelegramAccountRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<UserDepartment>, UserDepartmentRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<UserDepartment>, UserDepartmentRepository>();
+
+builder.Services.AddScoped<IAuditableCommandRepository<User>, UserRepository>();
+builder.Services.AddScoped<IAuditableQueryRepository<User>, UserRepository>();
 
 # region Logger configuration
 
@@ -22,9 +81,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PostgreSqlTmsContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql("Host=localhost;Port=5432;Database=otusTMS;Username=postgres;Password=3353"));
+
 
 var app = builder.Build();
+app.RegisterControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Otus.TMS/Otus.TMS/ViewModels/BoardViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Otus.TMS.ViewModels
+{
+    public class BoardViewModel { public string Name { get; set; } }
+    public class BoardUserViewModel { public Guid BoardId { get; set; } public Guid UserId { get; set; } }
+    public class BoardUserRoleViewModel { public Guid BoardUserId { get; set; } public Guid RoleId { get; set; } }
+    public class ColumnViewModel { public Guid BoardId { get; set; } public string Name { get; set; } }
+    public class CommentViewModel { public Guid TaskId { get; set; } public string Text { get; set; } }
+    public class CompanyViewModel { public string Name { get; set; } }
+    public class CredentialHistoryViewModel { public Guid CredentialId { get; set; } public string Value { get; set; } }
+    public class CredentialViewModel { public Guid UserId { get; set; } public string PasswordHash { get; set; } }
+    public class DepartmentViewModel { public Guid CompanyId { get; set; } public string Name { get; set; } }
+    public class NotificationSettingViewModel { public bool EmailNotificationsEnabled { get; set; } public bool PushNotificationsEnabled { get; set; } public bool TelegramNotificationsEnabled { get; set; } }
+    public class PermissionViewModel { public string Name { get; set; } }
+    public class RolePermissionViewModel { public Guid RoleId { get; set; } public Guid PermissionId { get; set; } }
+    public class RoleViewModel { public string Name { get; set; } }
+    public class TaskViewModel { public Guid ColumnId { get; set; } public string Name { get; set; } }
+    public class TaskTypeViewModel { public string Name { get; set; } }
+    public class TelegramAccountViewModel { public Guid UserId { get; set; } public string ChatId { get; set; } }
+    public class UserDepartmentViewModel { public Guid UserId { get; set; } public Guid DepartmentId { get; set; } }
+    public class UserViewModel { public string FirstName { get; set; } public string LastName { get; set; } }
+
+}
