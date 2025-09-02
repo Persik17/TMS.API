991548833c70146f9bd1bf7ca952f8491eedaf0e
Aleksandr <kraichik03@mail.ru>
2025-09-02 21:28:13 +0300

    fix: обновил dto под фронт и актуальную визуализацию

---
+++ TMS.Application/TMS.Application.Dto/Task/CreatedTaskDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TMS.Application.Dto.Task
+{
+    public class CreatedTaskDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public Guid BoardId { get; set; }
+        public int? Priority { get; set; }
+        public int? StoryPoints { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid CreatorId { get; set; }
+        public Guid? AssigneeId { get; set; }
+        public Guid TaskTypeId { get; set; }
+        public Guid ColumnId { get; set; }
+    }
+}
--- TMS.Application/TMS.Application.Dto/Task/TaskCreateDto.cs
+++ TMS.Application/TMS.Application.Dto/Task/TaskCreateDto.cs
@@ -3,17 +3,10 @@
     public class TaskCreateDto
     {
         public string Name { get; set; }
-        public string Description { get; set; }
         public Guid BoardId { get; set; }
         public Guid CreatorId { get; set; }
         public Guid AssigneeId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public int StoryPoints { get; set; }
         public Guid TaskTypeId { get; set; }
-        public int Priority { get; set; }
-        public int Severity { get; set; }
-        public Guid? ParentTaskId { get; set; }
         public Guid ColumnId { get; set; }
     }
 }
