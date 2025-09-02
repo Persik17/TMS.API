b444c01090f211f67365c908ca79d1a35d3602cf
Aleksandr <kraichik03@mail.ru>
2025-09-02 21:29:35 +0300

    fix: актуальный маппинг и обновление viewModel

--- TMS.API/ViewModels/TaskType/TaskTypeViewModel.cs
+++ TMS.API/ViewModels/TaskType/TaskTypeViewModel.cs
@@ -4,7 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+        public string? Color { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime? UpdateDate { get; set; }
         public DateTime? DeleteDate { get; set; }
