d00b405180dc3b9206a0b8edfc5a5cd1a1d46010
nagithube <agalarovgera@mail.ru>
2025-09-04 22:26:01 +0300

    Новая миграция

---
+++ TMS.Infrastructure/TMS.Infrastructure.DataAccess/Migrations/20250904170649_AddNewColumnInTaskTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TMS.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnInTaskTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompletedTask",
+                table: "Tasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompletedTask",
+                table: "Tasks");
+        }
+    }
+}
